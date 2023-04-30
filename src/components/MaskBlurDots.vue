b3618cd2d1e9eef624f1ac8910ee4e645d322b96
arno-solo <arno756@outlook.com>
2023-04-30 08:37:48 +0800

    Add 2 examples

---
+++ src/components/MaskBlurDots.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="relative w-64 h-64 overflow-y-scroll">
+    <div class="leading-6">
+      <p>
+        <span class="text-lg font-semibold">The cat</span> sat on the mat, licking its paws. It was a sunny day, and the birds were singing in the trees. The cat was content to sit there and enjoy the weather.
+      </p>
+      <p>
+        After a while, the cat got up and stretched. It yawned and then started to walk away. It was time for the cat to go on an adventure.
+      </p>
+      <p>
+        The cat walked down the street, sniffing the air. It smelled something delicious, and it followed the smell to a nearby bakery. The cat peered through the window and saw a row of fresh-baked pastries. The cat's mouth watered.
+      </p>
+      <p>The cat went inside the bakery and rubbed against the baker's legs. The baker smiled and gave the cat a piece of pastry. The cat ate the pastry quickly and then asked for another one. The baker laughed and gave the cat another pastry.</p>
+    </div>
+    <div class="sticky bottom-0 left-0 w-full h-10 mask" />
+  </div>
+</template>
+
+<style scoped>
+.mask {
+  background-image: radial-gradient(
+    rgb(0, 0, 0, 0) 1px,
+    rgb(255, 255, 255) 1px
+  );
+  background-size: 4px 4px;
+  backdrop-filter: blur(3px);
+}
+</style>
---
+++ src/components/PulseAnimation.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const hasPulse = ref(false)
+const isInfinite = ref(true)
+
+function handleClick() {
+  hasPulse.value = true
+  setTimeout(() => {
+    hasPulse.value = false
+  }, 2000)
+}
+</script>
+
+<template>
+  <div class="flex flex-col gap-1">
+    <div class="flex justify-center items-center w-18 h-18 rounded-1 bg-primary">
+      <div
+        class="w-12 h-12 rounded-full bg-white"
+        :class="{
+          'pulse-animation': hasPulse,
+          'pulse-animation-infinite': isInfinite,
+        }"
+        @click="handleClick"
+      />
+    </div>
+    <div class="flex gap-1">
+      <input v-model="isInfinite" type="checkbox">
+      <p>Infinite</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.pulse-animation {
+  animation: pulse 2s;
+}
+
+.pulse-animation-infinite {
+  animation: pulse 2s infinite;
+}
+
+@keyframes pulse {
+  0% {
+    transform: scale(1);
+    box-shadow: 0 0 0 0 rgba(255, 255, 255, 0.7);
+  }
+  70% {
+    transform: scale(1.05);
+    box-shadow: 0 0 0 2rem rgba(255, 255, 255, 0);
+  }
+  100% {
+    transform: scale(1);
+    box-shadow: 0 0 0 0 rgba(255, 255, 255, 0);
+  }
+}
+</style>
